b49a6e2907a8898dc4c71a65dfa6759862dd51e7
Xavier Arias <xavier_rate@outlook.com>
2021-05-26 17:45:37 +1000

    Animation states work now yey gg

--- Assets/Handmade Maze/Scripts/AgentSmith.cs
+++ Assets/Handmade Maze/Scripts/AgentSmith.cs
@@ -12,7 +12,7 @@ namespace AI
         private NavMeshAgent agent;
         private Waypoint[] waypoints;
         private NavMeshLink jumpLink;
-        private Animator anim;
+        private Animator _anim;
 
         //Will give us a random waypoint in the array as a variable
         private Waypoint RandomPoint => waypoints[Random.Range(0, waypoints.Length)];
@@ -20,6 +20,8 @@ namespace AI
         // Start is called before the first frame update
         void Start()
         {
+            _anim = gameObject.GetComponent<Animator>();
+            jumpLink = gameObject.GetComponent<NavMeshLink>();
             agent = gameObject.GetComponent<NavMeshAgent>();
             // FindObjectsOfType gets every instance of this component in the scene
             waypoints = FindObjectsOfType<Waypoint>();
@@ -31,9 +33,22 @@ namespace AI
             // Has the agent reached it's position?
             if (!agent.pathPending && agent.remainingDistance < 0.1f)
             {
-                anim.SetTrigger("Walk");
                 // Tell the agent to move to a random position in the scene waypoints
                 agent.SetDestination(RandomPoint.Position);
+                _anim.SetTrigger("Walk");
+            }
+            else if(waypoints == null)
+            {
+                _anim.SetTrigger("Idle");
+            }
+                
+            if (agent.isOnOffMeshLink)
+            {
+                if (!_anim.GetCurrentAnimatorStateInfo(0).IsName("Jumpin"))
+                {
+                    _anim.SetTrigger("Jump");
+                }
+                
             }
         }
     }
